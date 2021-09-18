6b8e4044ed58390a067d8971f081336b0d168c75
Kostya <ko.klochkov@gmail.com>
2021-09-18 15:48:02 +0300

    new back

---
+++ php/loginadmin.php
@@ -0,0 +1,10 @@
+<?php
+$login = $_POST['login'];
+$pass = $_POST['pass'];
+require 'config.php';
+$sql = ("SELECT * FROM `users` WHERE `login` = '$login' AND `pass` = '$pass'");
+$rez = mysqli_query($link, $sql);
+$user = $rez->fetch_assoc();
+setcookie('user', $user['id'], time() + 3600, "/");
+header("Location: /superadmin/index.html");
+?>
